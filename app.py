63387a65acb6fff6a50fad668b90e991c3477c41
Ethan <ethanjsummers00@gmail.com>
2022-08-24 21:52:39 -0700

    Weather finished
    - Finished pull and display of local weather data from location set in config.ini

--- app.py
+++ app.py
@@ -1,33 +1,63 @@
 from configparser import ConfigParser
 import configparser
 from distutils.command.config import config
+import requests
+from bs4 import BeautifulSoup
 
 configFile = 'config.ini'
 config = ConfigParser()
 config.read(configFile)
 
-print(config.sections())
-print(list(config['preferences']))
+# print(config.sections())
+# print(list(config['preferences']))
 
-nameConfig = config['preferences']
+pullConfig = config['preferences']
 
-if ((nameConfig['name']) == "null"):
+if ((pullConfig['name']) == "null"):
     nameInput = input("What is your first name?: ")
     
     config.set('preferences', 'name', nameInput)
     with open(configFile, 'w') as configWrite:
         config.write(configWrite)
+    
+    print("Hello " + (config['preferences']['name']))
 else:
     print("Hello " + (config['preferences']['name']))
 
-locInput = input("What city are you in?: ")
+urlWeather = "https://www.google.com/search?q=" + "weather" + (pullConfig['location'])
 
-config.set('preferences', 'location', locInput)
-with open(configFile, 'w') as configWrite:
-    config.write(configWrite)
+if ((pullConfig['location']) == "null"):
+    locInput = input("What city are you in?: ")
     
-locConfig = config['preferences']
+    config.set('preferences', 'location', locInput)
+    with open(configFile, 'w') as configWrite:
+        config.write(configWrite)
+    
+    urlWeather = "https://www.google.com/search?q=" + "weather" + (pullConfig['location'])    
+else:
+    print(urlWeather)
 
-urlWeather = "https://www.google.com/search?q=" + "weather" + (locConfig['location'])
+html = requests.get(urlWeather).content
+soup = BeautifulSoup(html, 'html.parser')
 
-print(urlWeather)
+temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
+str = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
+
+data = str.split('\n')
+time = data[0]
+sky = data[1]
+
+# list having all div tags having particular clas sname
+listdiv = soup.findAll('div', attrs={'class': 'BNeawe s3v9rd AP7Wnd'})
+
+# particular list with required data
+strd = listdiv[5].text
+
+# formatting the string
+pos = strd.find('Wind')
+other_data = strd[pos:]
+
+print("Temperature is", temp)
+print("Time: ", time)
+print("Sky Description: ", sky)
+print(other_data)
